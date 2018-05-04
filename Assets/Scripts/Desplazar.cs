407d0dc572874e11d605b92b99ad2937b766ed17
SrManny <32618674+SrManny@users.noreply.github.com>
2018-05-04 13:59:46 +0200

    Mario+ animacion de saltar y correr + escenario
    MiniPrototipo de escenario con un cubo + mario y sus fbx de saltar y correr
    Falta poner el mario en el escenario y que corra.
    El Ahorasi.rar es el mario .obj su material y textura

---
+++ Assets/Scripts/Desplazar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desplazar : MonoBehaviour {
+
+    // Use this for initialization
+    public bool click;
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void OnMouseDown()
+    {
+        BoxCollider bx = GetComponent<BoxCollider>();
+        NewBehaviourScript.dir = transform.position + bx.center;
+    }
+}
